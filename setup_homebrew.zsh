d83129fc8753e84b17218fc174f717a927cb0389
Riku Manninen <38488460+RikuManninen@users.noreply.github.com>
2022-07-17 00:28:29 +0300

    add setup_homebrew.zsh and run it with dotbot

---
+++ setup_homebrew.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting homebrew setup >>>\n"
+
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
